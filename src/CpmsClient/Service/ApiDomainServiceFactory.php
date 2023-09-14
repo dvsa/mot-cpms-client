da21423b7ba42763b441429cdff61c722ab9dc8a
czarnowskim <m.czarnowski@kainos.com>
2023-09-14 15:32:03 +0200

    feat: Update php version to php 8.1 and fix deprecations (BL-15591)

--- src/CpmsClient/Service/ApiDomainServiceFactory.php
+++ src/CpmsClient/Service/ApiDomainServiceFactory.php
@@ -1,14 +1,10 @@
 <?php
-
 namespace CpmsClient\Service;
 
 use CpmsClient\Utility\Util;
-use Exception;
-use Psr\Container\ContainerExceptionInterface;
-use Psr\Container\ContainerInterface;
-use Laminas\Http\PhpEnvironment\Request;
+use Interop\Container\ContainerInterface;
+use Laminas\Http\Request;
 use Laminas\ServiceManager\Factory\FactoryInterface;
-use Psr\Container\NotFoundExceptionInterface;
 
 /**
  * Class ApiDomainServiceFactory
@@ -20,48 +16,49 @@ class ApiDomainServiceFactory implements FactoryInterface
     /**
      * Create service
      *
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
-
-     * Required suppression due to un-typed parameter in parent class
+     * @param ContainerInterface $container
      *
-     * @psalm-suppress MissingParamType
-     * @psalm-suppress InvalidReturnType
-     * @phpstan-ignore-next-line
+     * @param $requestedName
+     * @param array|null $options
+     * @return mixed
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
      */
-    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null): string
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        /** @var array $config */
+
         $config = $container->get('config');
 
         if (empty($config['cpms_api']['rest_client']['options']['domain'])) {
+            /** @var \Laminas\Http\PhpEnvironment\Request $request */
             $request   = $container->get('request');
             $apiDomain = $this->determineLocalDomain($request, $config);
         } else {
             $apiDomain = $config['cpms_api']['rest_client']['options']['domain'];
         }
 
-        return Util::appendQueryString($apiDomain);
+        $apiDomain = Util::appendQueryString($apiDomain);
+
+        return $apiDomain;
     }
 
     /**
      * Determine the CPMS API domain if not set in the config
-     * @throws Exception
+     *
+     * @param $request
+     * @param $config
+     *
+     * @return mixed
      */
-    public function determineLocalDomain(mixed $request, array $config): mixed
+    public function determineLocalDomain($request, $config)
     {
+        /** @var \Laminas\Http\PhpEnvironment\Request $request */
         if ($request instanceof Request) {
             $currentDomain = $request->getServer('HTTP_HOST');
         } else {
             $currentDomain = $config['cpms_api']['home_domain'];
         }
 
-        // This check is to set a default value that prevents str_replace being called with a null value
-        // Passing null to the $subject parameter of str_replace is deprecated
-        if (!isset($currentDomain)) {
-            $currentDomain = '';
-        }
-
-        return str_replace('payment-app', 'payment-service', $currentDomain);
+        return str_replace('payment-app', 'payment-service', $currentDomain ?? '');
     }
 }
