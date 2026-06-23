2db8161a714070c118db17424b9f630ea7cf9ab0
Darren Whittington <darren.whittington@cgi.com>
2026-06-23 14:21:22 +0100

    fix: added global config file

---
+++ config/autoload/cpms-client.global.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'cpms_client' => [
+        'logger' => [
+            'location' => 'data/logs',
+            'filename' => 'cpms-client.log',
+            'channel' => 'cpms-client',
+        ],
+    ],
+];
+
